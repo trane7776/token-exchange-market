cf2eededf6f8b02bf5a5b46e390ed4e1e5e76195
trane7776 <izbasarov.t@edu.rea.ru>
2024-08-21 22:48:50 +0300

    tested

---
+++ scripts/1_deploy.js
@@ -0,0 +1,20 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require('hardhat');
+
+async function main() {
+  const Token = await ethers.getContractFactory('Token');
+  const token = await Token.deploy();
+  await token.deployed();
+  console.log('Token deployed to:', token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
