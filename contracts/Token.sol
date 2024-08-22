e948f16b642c6162672ed69f4151c9e6769b41f3
trane7776 <izbasarov.t@edu.rea.ru>
2024-08-22 15:00:11 +0300

    added transfer function

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,7 +11,7 @@ contract Token {
 
     // Track balances
     mapping(address => uint256) public balanceOf;
-
+    event Transfer(address indexed from, address indexed to, uint256 value);
     // Send tokens
 
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
@@ -19,7 +19,22 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
-        
+
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // require that the sender has enough tokens
+        require(balanceOf[msg.sender] >= _value, "Not enough tokens");
+        require(balanceOf[_to] + _value >= balanceOf[_to], "Overflow error");
+        require(_to != address(0), "Invalid recipient.");
+        // deduct tokens from spender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        // credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+        // emit transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
     
 }
--- test/Token.js
+++ test/Token.js
@@ -3,10 +3,10 @@ const { ethers } = require('hardhat');
 const tokens = (n) => ethers.utils.parseUnits(n.toString(), 'ether');
 describe('Token', () => {
   let token;
-  let deployer;
+  let deployer, person1;
   beforeEach(async () => {
     // Deploy the contract
-    [deployer] = await ethers.getSigners();
+    [deployer, person1] = await ethers.getSigners();
     const Token = await ethers.getContractFactory('Token');
     token = await Token.connect(deployer).deploy('Trane Coin', 'TRC', 1000000);
     await token.deployed();
@@ -36,10 +36,46 @@ describe('Token', () => {
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply);
     });
   });
+  describe('Sending Tokens', () => {
+    let amount, transaction, result;
+    describe('Success', () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .transfer(person1.address, amount);
+        result = await transaction.wait();
+      });
 
-  // describe spending
-
-  // describe approving
-
-  // describe transferring
+      it('transfers token balances', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(
+          tokens(999900)
+        );
+        expect(await token.balanceOf(person1.address)).to.equal(amount);
+      });
+      it('emits Transfer event', async () => {
+        const eventLog = result.events[0];
+        expect(eventLog.event).to.equal('Transfer');
+        const args = eventLog.args;
+        expect(args.from).to.equal(deployer.address);
+        expect(args.to).to.equal(person1.address);
+        expect(args.value).to.equal(amount);
+      });
+    });
+    describe('Failure', () => {
+      it('rejects insufficient balances', async () => {
+        // Transfer more than the balance of deployer - 100 millions
+        const invalidAmount = tokens(100000000);
+        await expect(
+          token.connect(deployer).transfer(person1.address, invalidAmount)
+        ).to.be.revertedWith('Not enough tokens');
+      });
+      it('rejects invalid recipients', async () => {
+        const amount = tokens(100);
+        await expect(
+          token.connect(deployer).transfer(ethers.constants.AddressZero, amount)
+        ).to.be.revertedWith('Invalid recipient');
+      });
+    });
+  });
 });
