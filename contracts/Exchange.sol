6b906e49132decaa2e92194421cfc9aad10580cf
trane7776 <izbasarov.t@edu.rea.ru>
2024-08-27 00:06:04 +0300

    configured fill order and charge fee function

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -137,4 +137,50 @@ contract Exchange {
     );
 
   }
+
+  // -----------------------------------------
+  // Execute orders
+  // -----------------------------------------
+
+  function fillOrder(uint256 _id) public {
+    // fetch the order
+    _Order storage _order = orders[_id];
+
+
+    // Swapping Tokens (Trading)
+    _trade(
+      _order.id,
+      _order.user,
+      _order.tokenGet,
+      _order.amountGet,
+      _order.tokenGive,
+      _order.amountGive
+    );
+  }
+
+  function _trade(
+    uint256 _orderId,
+    address _user,
+    address _tokenGet,
+    uint256 _amountGet,
+    address _tokenGive,
+    uint256 _amountGive
+  ) internal {
+    // Fee paid by the user that fills the order, a.k.a. msg.sender
+    // Fee deducted from _amountGet
+    uint256 _feeAmount = _amountGet * feePercent / 100;
+    // Execute trade
+    // User that fills the order is msg.sender (the user that calls this function) 
+    tokens[_tokenGet][_user] = tokens[_tokenGet][_user] + _amountGet;
+    tokens[_tokenGet][msg.sender] =
+      tokens[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+    
+    // Charge fees
+    tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount] + _feeAmount;
+    
+    tokens[_tokenGive][_user] = tokens[_tokenGive][_user] - _amountGive;
+    tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender] + _amountGive;
+
+
+  }
 }
